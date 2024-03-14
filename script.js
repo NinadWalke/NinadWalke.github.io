e22393db597d3282decdd0c5225a91c2a191757d
NinadWalke <ninadwalke00@gmail.com>
2024-03-14 21:30:02 +0530

    Fixed the opacity 0 clickable bug using PointerEvents property of CSS in JavaScript

--- script.js
+++ script.js
@@ -22,21 +22,25 @@ function setExpHeader() {
     experience.style.fontSize = "35px";
     experience.style.borderBottom = "4px solid #64b9ba"
     experienceData.style.opacity = "100";
+    experienceData.style.pointerEvents = "auto";
     
     skills.style.fontSize = "22px";
     skillsHeader.style.color = "grey";
     skills.style.borderBottom = "none";
     skillsData.style.opacity = "0";
+    skillsData.style.pointerEvents = "none";
     
     projects.style.fontSize = "22px";
     projectsHeader.style.color = "grey";
     projects.style.borderBottom = "none";
     projectsData.style.opacity = "0";
+    projectsData.style.pointerEvents = "none";
 
     resume.style.fontSize = "22px";
     resumeHeader.style.color = "grey";
     resume.style.borderBottom = "none";
     resumeData.style.opacity = "0";
+    resumeData.style.pointerEvents = "none";
 }
 
 function setSkillsHeader() {
@@ -44,21 +48,25 @@ function setSkillsHeader() {
     experience.style.fontSize = "22px";
     experience.style.borderBottom = "none";
     experienceData.style.opacity = "0";
+    experienceData.style.pointerEvents = "none";
     
     skills.style.fontSize = "35px";
     skillsHeader.style.color = "#64b9ba";
     skills.style.borderBottom = "4px solid #64b9ba"
     skillsData.style.opacity = "100";
+    skillsData.style.pointerEvents = "auto";
     
     projects.style.fontSize = "22px";
     projectsHeader.style.color = "grey";
     projects.style.borderBottom = "none";
     projectsData.style.opacity = "0";
+    projectsData.style.pointerEvents = "none";
 
     resume.style.fontSize = "22px";
     resumeHeader.style.color = "grey";
     resume.style.borderBottom = "none";
     resumeData.style.opacity = "0";
+    resumeData.style.pointerEvents = "none";
 }
 
 function setProjecsHeader() {
@@ -66,21 +74,25 @@ function setProjecsHeader() {
     experience.style.fontSize = "22px";
     experience.style.borderBottom = "none";
     experienceData.style.opacity = "0";
+    experienceData.style.pointerEvents = "none";
     
     skills.style.fontSize = "22px";
     skillsHeader.style.color = "grey";
     skills.style.borderBottom = "none";
     skillsData.style.opacity = "0";
+    skillsData.style.pointerEvents = "none";
     
     projects.style.fontSize = "35px";
     projectsHeader.style.color = "#64b9ba";
     projects.style.borderBottom = "4px solid #64b9ba"
     projectsData.style.opacity = "100";
+    projectsData.style.pointerEvents = "auto";
 
     resume.style.fontSize = "22px";
     resumeHeader.style.color = "grey";
     resume.style.borderBottom = "none";
     resumeData.style.opacity = "0";
+    resumeData.style.pointerEvents = "none";
 }
 
 function setResumeHeader() {
@@ -88,21 +100,25 @@ function setResumeHeader() {
     experience.style.fontSize = "22px";
     experience.style.borderBottom = "none";
     experienceData.style.opacity = "0";
+    experienceData.style.pointerEvents = "none";
     
     skills.style.fontSize = "22px";
     skillsHeader.style.color = "grey";
     skills.style.borderBottom = "none";
     skillsData.style.opacity = "0";
+    skillsData.style.pointerEvents = "none";
     
     projects.style.fontSize = "22px";
     projectsHeader.style.color = "grey";
     projects.style.borderBottom = "none";
     projectsData.style.opacity = "0";
+    projectsData.style.pointerEvents = "none";
 
     resume.style.fontSize = "35px";
     resumeHeader.style.color = "#64b9ba";
     resume.style.borderBottom = "4px solid #64b9ba"
     resumeData.style.opacity = "100";
+    resumeData.style.pointerEvents = "auto";
 }
 /*
     color: #64b9ba;
